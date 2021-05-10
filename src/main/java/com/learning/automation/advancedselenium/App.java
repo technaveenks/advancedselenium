f2f9eea13de03f68be4ffeef1cda93a6d60b58de
techfundas <tech.naveenks@gmail.com>
2021-05-10 08:54:03 +0530

    Initial Commit

---
+++ src/main/java/com/learning/automation/advancedselenium/App.java
@@ -0,0 +1,13 @@
+package com.learning.automation.advancedselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
